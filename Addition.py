9177f7f38704fb6b8d120f19512694cb7814712b
guoqy24 <53734990+guoqy24@users.noreply.github.com>
2021-07-26 17:35:30 +0800

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -1,8 +1,13 @@
 # -*- coding: UTF-8 -*-
 
 
-num1 = input('输入第一个数字：')
-num2 = input('输入第二个数字：')
+#num1 = input('输入第一个数字：')
+from StdSuites import data
+
+num1=data.get_parameters_from_jenkins("num1")
+num2=data.get_parameters_from_jenkins("num2")
+
+#num2 = input('输入第二个数字：')
 
 # 求和
 sum = float(num1) + float(num2)
